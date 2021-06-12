b8868ac90f3a9f7c1ea6c1f98fec58040fd254ad
RabidMexican <rabidmexican@gmail.com>
2021-06-12 11:32:31 +0200

    reworked the update script to do everything at once

--- anzere/scripts/update_data.py
+++ anzere/scripts/update_data.py
@@ -1,26 +1,32 @@
 import csv
 
-TABLE = input('Enter table name : ')
+TABLES = ['commerce', 'gare', 'parking', 'piste', 'telecabine', 'teleski', 'telesiege']
 LAST_COLUMN = 9
-REQUEST_START = 'UPDATE ' + TABLE + ' SET '
 
-with open('data.csv', newline='') as csvfile:
-    i = 1
-    reader = csv.reader(csvfile, delimiter=',')
 
-    for row in reader:
-        t = 0
-        request = REQUEST_START
+def print_request_for_table(current_table):
+    request_start = 'UPDATE ' + current_table + ' SET '
+    with open('./data/{0}.csv'.format(current_table), newline='') as csvfile:
+        i = 1
+        reader = csv.reader(csvfile, delimiter=',')
 
-        for data in row:
-            data = data.replace('%', '')
-            request = '{0} t_{1} = {2}'.format(request, t, data)
+        for row in reader:
+            t = 0
+            request = request_start
 
-            if t < LAST_COLUMN:
-                request = request + ','
+            for data in row:
+                data = data.replace('%', '')
+                request = '{0} t_{1} = {2}'.format(request, t, data)
 
-            t += 1
+                if t < LAST_COLUMN:
+                    request = request + ','
 
-        request = '{0} WHERE id = {1}{2}'.format(request, i, ';')
-        i += 1
-        print(request)
+                t += 1
+
+            request = '{0} WHERE id = {1}{2}'.format(request, i, ';')
+            i += 1
+            print(request)
+
+
+for table in TABLES:
+    print_request_for_table(table)
