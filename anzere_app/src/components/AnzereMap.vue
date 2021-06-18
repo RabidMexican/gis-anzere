eabd308ae6c629c45d0f782571df9849738d6481
RabidMexican <39406726+RabidMexican@users.noreply.github.com>
2021-06-18 21:22:57 +0200

    tweaked traffic colors

--- anzere_app/src/components/AnzereMap.vue
+++ anzere_app/src/components/AnzereMap.vue
@@ -166,8 +166,8 @@ export default {
       loaded: false,
       busy: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
       percentColors : [
-        { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
-        { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+        { pct: 0, color: { r: 0xff, g: 0x00, b: 0 } },
+        { pct: 0.2, color: { r: 0xff, g: 0xff, b: 0 } },
         { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } } 
       ],
 
---
+++ anzere_app/src/components/Chart.vue
@@ -0,0 +1,27 @@
+<script>
+import Line from 'vue-chartjs'
+export default {
+  extends: Line,
+  name: "Chart",
+  components: {
+    LineChart: Line
+  },
+  props: {
+    data: {
+      require: true,
+      type: Object,
+    },
+    options: {
+      require: false,
+      type: Object,
+    }
+  },
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
